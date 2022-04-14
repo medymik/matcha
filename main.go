8923eb110d32e64d70cc0dd638cbafcb8398394b
medymik <mohammed.ymik@outlook.com>
2022-04-14 04:15:43 +0000

    feat: setup swagger docs

--- main.go
+++ main.go
@@ -1,12 +1,36 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	docs "github.com/medymik/matcha/docs"
+	ginSwagger "github.com/swaggo/gin-swagger"
+	"github.com/swaggo/gin-swagger/swaggerFiles"
+)
+
+// @BasePath /api/v1
+// @Summary ping health
+// @Schemes
+// @Description do ping
+// @Tags Health
+// @Accept json
+// @Produce json
+// @Success 200 {string} pong
+// @Router /health/ping [get]
+func HealthPing(ctx *gin.Context) {
+	ctx.String(200, "pong")
+}
 
 func setupRouter() *gin.Engine {
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.String(200, "pong")
-	})
+	docs.SwaggerInfo.BasePath = "/api/v1"
+	v1 := r.Group("/api/v1")
+	{
+		eg := v1.Group("/health")
+		{
+			eg.GET("/ping", HealthPing)
+		}
+	}
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 	return r
 }
 
